ab2529a293e0e4949d4419d7f670b9ed866efc8c
Logan Zehr <zehrl315@outlook.com>
2020-11-01 17:54:06 -0800

    Decreased js clutter

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 // DOM Initializing
 var timeEl = document.querySelector("#time")
+var viewHighscoresEl = document.querySelector("#viewHighscores")
 
 var startMenuEl = document.querySelector(".startMenu");
 var startMenuBtn = document.querySelector("#startMenuBtn");
@@ -9,94 +10,115 @@ var questionEl = document.querySelector("#question");
 var optionParentEl = document.querySelector("#optionParent");
 
 var quizEndMenuEl = document.querySelector(".quizEndMenu");
+var submitInitialsBtn = document.querySelector("#submitInitialsBtn");
 
-var highScoreMenuEl = document.querySelector(".highScoreMenu")
-
-
-
-
-
+var highScoreMenuEl = document.querySelector(".highScoreMenu");
+var goBackBtn = document.querySelector("#goBackBtn");
 
 // Initialize Variables
-var score = 0;
+var score;
 var secondsRemaining;
+var timer;
+var questions;
 
-var questions = [
-    {
-        question: "What's your favorite color?",
-        answers: ["blue", "red", "yellow", "green"],
-        correctAnswer: "blue"
-    },
-];
+function initializeQuestions() {
+    questions = [
+        {
+            question: "What's your favorite color?",
+            answers: ["blue", "red", "yellow", "green"],
+            correctAnswer: "blue"
+        },
+        {
+            question: "How do you create a comment in Javascript?",
+            answers: ["//", "##", "~", "-"],
+            correctAnswer: "//"
+        },
+        {
+            question: "How do you initialize a variable?",
+            answers: ["var x = 1;", "set x = 1;", "dim x = 1", "by the power of the gods, x shall be 1"],
+            correctAnswer: "var x = 1"
+        },
+        {
+            question: "What year was Javascript invented?",
+            answers: ["1995", "2000", "2005", "2010"],
+            correctAnswer: "1995"
+        },
+    ];
+}
 
-// TESTS
+
 function startQuiz() {
-    hideMenu(startMenuEl);
-    showMenu(questionCardEl);
+    score = 0;
+    secondsRemaining = 30;
+    initializeQuestions();
+
+    changeMenu(questionCardEl);
     nextQuestion();
 
-    secondsRemaining = 30;
     timeEl.textContent = secondsRemaining;
-    setInterval(function () {
+    timer = setInterval(function () {
         secondsRemaining--;
         timeEl.textContent = secondsRemaining;
         if (secondsRemaining <= 0) {
-            hideMenu(questionCardEl);
-            showMenu(quizEndMenuEl)
+            changeMenu(quizEndMenuEl);
+            clearInterval(timer);
         }
     }, 1000);
 
 }
 
 function nextQuestion() {
+    console.log("Fired!")
+    
+    // end quiz if there are no questions left
+    if (questions.length <= 0) {
+        clearInterval(timer);
+        changeMenu(highScoreMenuEl);
+        return;
+    }
+    
+    var randIndex = Math.floor(questions.length * Math.random())
+    console.log(`randIndex = ${randIndex}`);
+
     //select random question
-    questionEl.textContent = questions[0].question;
-    for (let i = 0; i < questions[0].answers.length; i++) {
-        optionParentEl.children[i].textContent = questions[0].answers[i];
+    questionEl.textContent = questions[randIndex].question;
+    for (let i = 0; i < questions[randIndex].answers.length; i++) {
+        optionParentEl.children[i].textContent = questions[randIndex].answers[i];
     }
 
 
 
-    //remove it from the 
+    //remove it from the list
+    questions.splice(randIndex, 1);
 
-    //if on start menu
-    //then 
-    //if on questionCard then change question
 }
 
-function hideMenu(element) {
-    element.style.display = "None"
+function submitInitials() {
+    //do stuff here
+
+    // Change to highscore menu
+    changeMenu(highScoreMenuEl);
 }
 
-function showMenu(element) {
+function changeMenu(element) {
+    startMenuEl.style.display = "None";
+    questionCardEl.style.display = "None";
+    quizEndMenuEl.style.display = "None";
+    highScoreMenuEl.style.display = "None";
     element.style.display = "inline-block"
 }
 
-// TESTS
-
-// Fill in questions
-
-
-// Display start button and wait for user to click start button
-
-// User clicks start button, hide start menu elements
-
-// Display question and 4 options and wait for user to click an option
-// question[0]
-
-// User clicks an answer, display "Wrong" or "Correct"
-// update score if correct
-// decrease time if incorrect
-
-// Display question and 4 options and wait for user to click an option
-// question [1]
-
-// ...
-
-
-// When user clicks start button, start timer "Set time -> set interval"
-
-
 // ------ EVENT HANDLERS ------
 startMenuBtn.addEventListener("click", startQuiz)
+
 optionParentEl.addEventListener("click", nextQuestion)
+
+submitInitialsBtn.addEventListener("click", submitInitials)
+
+viewHighscoresEl.addEventListener("click", function () {
+    changeMenu(highScoreMenuEl);
+})
+
+goBackBtn.addEventListener("click", function () {
+    changeMenu(startMenuEl);
+})
